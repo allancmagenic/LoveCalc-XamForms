4b33622fb43c353509ee69a74c8431c30547f71d
AllanC <allanc@magenic.com>
2018-07-18 14:44:59 +0800

    initial commit

---
+++ LoveCalculator/LoveCalculator/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoveCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateFlames_Clicked()
+        {
+            
+        }
+    }
+}
